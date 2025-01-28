2e408183b94d61024b99e92f6bd77d625fbbbcd9
Irwnda <irwandaa7@gmail.com>
2025-01-28 12:26:59 +0700

    feat: make the bot scroll when reach the bottom (LinkedIn shows 10 likes each scroll)

--- from_like.py
+++ from_like.py
@@ -3,7 +3,12 @@ import time
 
 from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.common import ElementNotInteractableException, StaleElementReferenceException, NoSuchElementException, \
+    NoSuchWindowException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 load_dotenv()
 driver = webdriver.Chrome()
@@ -40,20 +45,50 @@ def open_profile(link):
             driver.switch_to.window(handle)
             break
 
-    connect()
-    driver.close()
+    try:
+        connect()
+        driver.close()
+    except ElementNotInteractableException:
+        print("Element not interactable")
+    except NoSuchWindowException:
+        print("Window already closed")
+
     driver.switch_to.window(original_window)
 
 def show_likes():
     driver.find_element(By.XPATH, "//button[@data-reaction-details]").click()
+    like_count_element = driver.find_element(By.CLASS_NAME, 'social-details-social-counts__social-proof-fallback-number')
+    like_count = int(like_count_element.text)
+    visited_account_count = 0
     time.sleep(2)
 
-    reactors = driver.find_elements(By.CLASS_NAME, "social-details-reactors-tab-body-list-item")
-    for reactor in reactors:
-        link = reactor.find_element(By.CSS_SELECTOR, "a")
-        if '1st' in link.text:
+    while visited_account_count < like_count:
+        try:
+            scrollable_container = driver.find_element(By.CLASS_NAME, "social-details-reactors-tab-body")
+            reactors = driver.find_elements(By.CLASS_NAME, "social-details-reactors-tab-body-list-item")
+            while visited_account_count < len(reactors):
+                reactor = reactors[visited_account_count]
+
+                link = reactor.find_element(By.CSS_SELECTOR, "a")
+                if '1st' in link.text:
+                    visited_account_count += 1
+                    continue
+
+                open_profile(link)
+                visited_account_count += 1
+        except StaleElementReferenceException:
+            print("Stale element encountered. Retrying...")
             continue
-        open_profile(link)
+        except NoSuchElementException:
+            print("No element encountered. Retrying...")
+            driver.close()
+            continue
+
+        driver.execute_script(
+            "document.querySelector('.social-details-reactors-tab-body').parentElement.scroll(0, arguments[0].offsetHeight);",
+            scrollable_container
+        )
+        time.sleep(3)
 
 def check_confirmation():
     time.sleep(2)
@@ -63,8 +98,9 @@ def check_confirmation():
             button.click()
 
 def connect():
-    time.sleep(5)
-    action_buttons_wrapper = driver.find_element(By.CLASS_NAME, "umjiWHzhkMWxuVrcgedbAbMsQbYPbDdZ")
+    action_buttons_wrapper = WebDriverWait(driver, 5).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "umjiWHzhkMWxuVrcgedbAbMsQbYPbDdZ"))
+    )
     action_buttons = action_buttons_wrapper.find_elements(By.TAG_NAME, "button")
 
     connect_button = None
@@ -89,7 +125,13 @@ def connect():
     li_elements = dropdown.find_elements(By.TAG_NAME, 'li')
     for li_element in li_elements:
         div_element = li_element.find_element(By.TAG_NAME, 'div')
-        if 'connect' in div_element.get_attribute('aria-label'):
+        try:
+            if 'connect' in div_element.get_attribute('aria-label'):
+                div_element.click()
+                check_confirmation()
+                break
+        except ElementNotInteractableException:
+            more_button.click()
             div_element.click()
             check_confirmation()
             break
